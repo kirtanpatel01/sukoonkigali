c487353af9512886cf40e4af258f33d654532330
Kirtan Patel <kjpatel200022@gmail.com>
2026-08-22 02:49:14 +0530

    minor updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sukoonkigali.com',
   vite: {
     plugins: [tailwindcss()]
   }
 });
-
