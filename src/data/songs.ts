4680cbc24eb99798b86f3351702efb338705a4e1
Kirtan Patel <kjpatel200022@gmail.com>
2026-08-27 03:17:56 +0530

    add other 105 songs in total

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -8,9 +8,49 @@ export const PLAYLIST_ID = 'PLUKBMAqy5FT0';
 
 export const SONGS: Song[] = [
   {
-    youtubeId: 'JKSoBqnQ5I4',
-    title: 'Iktara',
-    artist: 'Amit Trivedi, Kavita Seth'
+    youtubeId: 'KlbACbvPCFE',
+    title: 'A Thousand Years (Slowed + Reverb)',
+    artist: 'Christina Perri'
+  },
+  {
+    youtubeId: 'PwEvz2FLZ4I',
+    title: 'Aankhon Mein Doob Jaane Ko',
+    artist: 'THE 9TEEN'
+  },
+  {
+    youtubeId: '2vKMY75kvjI',
+    title: 'Aankhon Se Batana',
+    artist: 'Dikshant'
+  },
+  {
+    youtubeId: 'SMlGGRAB3Hc',
+    title: 'Afreen Afreen',
+    artist: 'Rahat Fateh Ali Khan, Momina Mustehsan'
+  },
+  {
+    youtubeId: 'y1AhnOVOKSU',
+    title: 'Agar Tu Hota',
+    artist: 'Ankit Tiwari'
+  },
+  {
+    youtubeId: 'FOA9iyxsW_A',
+    title: 'Agar Tum Saath Ho',
+    artist: 'Arijit Singh, Alka Yagnik, A.R. Rahman'
+  },
+  {
+    youtubeId: 'Kg8_ADYhYWE',
+    title: 'Arz Kiya Hai',
+    artist: 'Anuv Jain'
+  },
+  {
+    youtubeId: 'T-g39o0rDos',
+    title: 'Aye Khuda',
+    artist: 'Salim Merchant, Salim-Sulaiman'
+  },
+  {
+    youtubeId: 'NLKwRW2y-sg',
+    title: 'Baarishein',
+    artist: 'Anuv Jain'
   },
   {
     youtubeId: 'vsHtDl4Wee4',
@@ -23,134 +63,14 @@ export const SONGS: Song[] = [
     artist: 'Banjaare'
   },
   {
-    youtubeId: 'Kg8_ADYhYWE',
-    title: 'Arz Kiya Hai',
-    artist: 'Anuv Jain'
+    youtubeId: 'Dorfmg7buTE',
+    title: 'Besabriyaan',
+    artist: 'Armaan Malik, Amaal Mallik'
   },
   {
-    youtubeId: 'LUgpPmj6nR8',
-    title: 'Khat',
-    artist: 'Navjot Ahuja'
-  },
-  {
-    youtubeId: 'LV_wiOhO40Q',
-    title: 'Dooron Dooron',
-    artist: 'Paresh Pahuja, Harleen Sethi'
-  },
-  {
-    youtubeId: 'gKD1AhmpOoU',
-    title: 'Ishq',
-    artist: 'Faheem Abdullah, Rauhan Malik'
-  },
-  {
-    youtubeId: 'IltsOcCj1Ak',
-    title: 'Haseen',
-    artist: 'Talwiinder, NDS, Rippy'
-  },
-  {
-    youtubeId: '3Cp2QTBZAFQ',
-    title: 'Finding Her',
-    artist: 'Kushagra, Vanshika'
-  },
-  {
-    youtubeId: 'tpuAUPu6FCA',
-    title: 'Sajna Ve',
-    artist: 'Aditya Rikhari, Ravator'
-  },
-  {
-    youtubeId: 'Es4NrOnoNb4',
-    title: 'Paro',
-    artist: 'Aditya Rikhari'
-  },
-  {
-    youtubeId: 'cStOvI74hQo',
-    title: 'Dekha Hi Nahi',
-    artist: 'Osho Jain'
-  },
-  {
-    youtubeId: '9UxU0qsfGPA',
-    title: 'Lamhey',
-    artist: 'Anubha Bajaj'
-  },
-  {
-    youtubeId: 'AsEd5OAys9E',
-    title: 'Jogi',
-    artist: 'Thiarajxtt, Bir'
-  },
-  {
-    youtubeId: 'Qwm6BSGrOq0',
-    title: 'Iraaday',
-    artist: 'Abdul Hannan, Rovalio'
-  },
-  {
-    youtubeId: '0IIJxkDtkHY',
-    title: 'Husn',
-    artist: 'Anuv Jain'
-  },
-  {
-    youtubeId: 'ilNt2bikxDI',
-    title: 'Jo Tum Mere Ho',
-    artist: 'Anuv Jain'
-  },
-  {
-    youtubeId: 'SmaY7RfBgas',
-    title: 'Gul',
-    artist: 'Anuv Jain'
-  },
-  {
-    youtubeId: 'EiiOYwqk3A0',
-    title: 'Faasle',
-    artist: 'Aditya Rikhari'
-  },
-  {
-    youtubeId: 'BcSejVIxB0E',
-    title: 'Ishq Hai',
-    artist: 'Anurag Saikia'
-  },
-  {
-    youtubeId: 'HrnrqYxYrbk',
-    title: 'Satranga',
-    artist: 'Arijit Singh, Shreyas P'
-  },
-  {
-    youtubeId: 'Etkd-07gnxM',
-    title: 'O Maahi',
-    artist: 'Arijit Singh, Pritam'
-  },
-  {
-    youtubeId: '2vKMY75kvjI',
-    title: 'Aankhon Se Batana',
-    artist: 'Dikshant'
-  },
-  {
-    youtubeId: 'AFTIVN8rRbI',
-    title: 'Sajni',
-    artist: 'Arijit Singh, Ram Sampath'
-  },
-  {
-    youtubeId: 'TjXH_P7Khhg',
-    title: 'Ve Kamleya',
-    artist: 'Arijit Singh, Shreya Ghoshal'
-  },
-  {
-    youtubeId: 'AX6OrbgS8lI',
-    title: 'Tu Hai Kahan',
-    artist: 'AUR'
-  },
-  {
-    youtubeId: 'NLKwRW2y-sg',
-    title: 'Baarishein',
-    artist: 'Anuv Jain'
-  },
-  {
-    youtubeId: 'iOIF74Hk80A',
-    title: 'Kasoor',
-    artist: 'Prateek Kuhad'
-  },
-  {
-    youtubeId: 'U2SVCCENLjE',
-    title: 'Co2',
-    artist: 'Prateek Kuhad'
+    youtubeId: '_51KXfwcPMs',
+    title: 'Bulleya',
+    artist: 'Papon, Vishal & Shekhar'
   },
   {
     youtubeId: '7c3-Gei5j4w',
@@ -158,13 +78,463 @@ export const SONGS: Song[] = [
     artist: 'Aditya A.'
   },
   {
-    youtubeId: 'KUpwupYj_tY',
-    title: 'Tere Hawaale',
+    youtubeId: 'HjKSkhqK4WE',
+    title: 'Chal Wahan Jaate Hain',
+    artist: 'Arijit Singh, Amaal Mallik'
+  },
+  {
+    youtubeId: 'Vx7YkKpt-J4',
+    title: 'Chidiya',
+    artist: 'Vilen'
+  },
+  {
+    youtubeId: 'opwZ_PJ-F_E',
+    title: 'Choo Lo',
+    artist: 'The Local Train'
+  },
+  {
+    youtubeId: 'U2SVCCENLjE',
+    title: 'Co2',
+    artist: 'Prateek Kuhad'
+  },
+  {
+    youtubeId: 'Rv1ZErn_PC8',
+    title: 'Daayre',
+    artist: 'Arijit Singh, Pritam'
+  },
+  {
+    youtubeId: '7eEysaev0kw',
+    title: 'Darkhaast',
+    artist: 'Arijit Singh, Sunidhi Chauhan, Mithoon'
+  },
+  {
+    youtubeId: 'kceO1AMi-mE',
+    title: 'Darmiyaan',
+    artist: 'Shafqat Amanat Ali, Salim-Sulaiman'
+  },
+  {
+    youtubeId: 'EYXBWSPXcZw',
+    title: 'Deewana Kar Raha Hai',
+    artist: 'Javed Ali'
+  },
+  {
+    youtubeId: 'cStOvI74hQo',
+    title: 'Dekha Hi Nahi',
+    artist: 'Osho Jain'
+  },
+  {
+    youtubeId: 'maKDIvUVkQo',
+    title: 'Dil Kyun Yeh Mera',
+    artist: 'KK, Pritam'
+  },
+  {
+    youtubeId: 'LV_wiOhO40Q',
+    title: 'Dooron Dooron',
+    artist: 'Paresh Pahuja, Harleen Sethi'
+  },
+  {
+    youtubeId: '8gq3S53aH3Y',
+    title: 'Ehsaas',
+    artist: 'Faheem Abdullah'
+  },
+  {
+    youtubeId: 'EiiOYwqk3A0',
+    title: 'Faasle',
+    artist: 'Aditya Rikhari'
+  },
+  {
+    youtubeId: '3Cp2QTBZAFQ',
+    title: 'Finding Her',
+    artist: 'Kushagra, Vanshika'
+  },
+  {
+    youtubeId: 'PZtSnQBsBW0',
+    title: 'Finding Her',
+    artist: 'Kushagra, Vanshika'
+  },
+  {
+    youtubeId: '-9fMvHicwLE',
+    title: 'Girl I Need You',
+    artist: 'Arijit Singh, Meet Bros, Roach Killa'
+  },
+  {
+    youtubeId: 'SmaY7RfBgas',
+    title: 'Gul',
+    artist: 'Anuv Jain'
+  },
+  {
+    youtubeId: '68Xh6j__cSM',
+    title: 'Hasi (Male Version)',
+    artist: 'Ami Mishra'
+  },
+  {
+    youtubeId: '8v-TWxPWIWc',
+    title: 'Humsafar',
+    artist: 'Akhil Sachdeva, Mansheel Gujral'
+  },
+  {
+    youtubeId: '0IIJxkDtkHY',
+    title: 'Husn',
+    artist: 'Anuv Jain'
+  },
+  {
+    youtubeId: 'JKSoBqnQ5I4',
+    title: 'Iktara',
+    artist: 'Amit Trivedi, Kavita Seth'
+  },
+  {
+    youtubeId: 'Qwm6BSGrOq0',
+    title: 'Iraaday',
+    artist: 'Abdul Hannan, Rovalio'
+  },
+  {
+    youtubeId: 'gKD1AhmpOoU',
+    title: 'Ishq',
+    artist: 'Faheem Abdullah, Rauhan Malik'
+  },
+  {
+    youtubeId: 'BcSejVIxB0E',
+    title: 'Ishq Hai',
+    artist: 'Anurag Saikia'
+  },
+  {
+    youtubeId: 'TQqBjSAK52s',
+    title: 'Ishq Hai',
+    artist: 'Anurag Saikia'
+  },
+  {
+    youtubeId: '5RT6QMKJTjQ',
+    title: 'Ishq Sufiyana',
+    artist: 'Kamal Khan, Vishal-Shekhar'
+  },
+  {
+    youtubeId: '3HlzLiCL9Xs',
+    title: 'Jaan Nisaar',
+    artist: 'Arijit Singh, Amit Trivedi'
+  },
+  {
+    youtubeId: '0mQLTWvXbXM',
+    title: 'Jab Tak',
+    artist: 'Armaan Malik, Amaal Mallik'
+  },
+  {
+    youtubeId: 'zFdi834FiZ4',
+    title: 'Jeena Jeena',
+    artist: 'Atif Aslam, Sachin-Jigar'
+  },
+  {
+    youtubeId: 'ilNt2bikxDI',
+    title: 'Jo Tum Mere Ho',
+    artist: 'Anuv Jain'
+  },
+  {
+    youtubeId: 'AsEd5OAys9E',
+    title: 'Jogi',
+    artist: 'Thiarajxtt, Bir'
+  },
+  {
+    youtubeId: 'lKSvy2jP1gc',
+    title: 'Kabhi Na Kabhi',
+    artist: 'Aditya Narayan'
+  },
+  {
+    youtubeId: 'WKv07mnKVEE',
+    title: 'Kaise Hua',
+    artist: 'Vishal Mishra'
+  },
+  {
+    youtubeId: 'yqpCGpyXO4g',
+    title: 'Kalank (Duet)',
     artist: 'Arijit Singh, Shilpa Rao, Pritam'
+  },
+  {
+    youtubeId: 'Grr0FlC8SQA',
+    title: 'Kalank Title Track',
+    artist: 'Arijit Singh, Pritam'
+  },
+  {
+    youtubeId: 'Hd1b_1a3J0A',
+    title: 'Kanha',
+    artist: 'Shreya Ghoshal, Sajid-Wajid'
+  },
+  {
+    youtubeId: 'iOIF74Hk80A',
+    title: 'Kasoor',
+    artist: 'Prateek Kuhad'
+  },
+  {
+    youtubeId: 'LUgpPmj6nR8',
+    title: 'Khat',
+    artist: 'Navjot Ahuja'
+  },
+  {
+    youtubeId: 'vt4jX0iRgCg',
+    title: 'Kho Gaye Hum Kahan',
+    artist: 'Jasleen Royal, Prateek Kuhad'
+  },
+  {
+    youtubeId: 'jq69R33z4hU',
+    title: 'Khudaya Khair',
+    artist: 'Soham Chakrabarthy, Akruti Kakkar, Monali Thakur'
   },
   {
     youtubeId: 'T94PHkuydcw',
     title: 'Kun Faya Kun',
     artist: 'A.R. Rahman, Javed Ali, Mohit Chauhan'
+  },
+  {
+    youtubeId: 'AAo7KNmZ7NE',
+    title: 'Lambiya Judaiyan',
+    artist: 'Bilal Saeed'
+  },
+  {
+    youtubeId: '9UxU0qsfGPA',
+    title: 'Lamhey',
+    artist: 'Anubha Bajaj'
+  },
+  {
+    youtubeId: '02VlO-J4AqY',
+    title: 'Long Time No See',
+    artist: 'Taimour Baig'
+  },
+  {
+    youtubeId: '9Zpmpln76vE',
+    title: 'Main Agar',
+    artist: 'Atif Aslam, Pritam'
+  },
+  {
+    youtubeId: 'MldmN-0b9j4',
+    title: 'Main Kho Gaya',
+    artist: 'Kushagra, Showkidd'
+  },
+  {
+    youtubeId: '4WB5y4DAcKE',
+    title: 'Main Rahoon Ya Na Rahoon',
+    artist: 'Armaan Malik, Amaal Mallik'
+  },
+  {
+    youtubeId: 's_GR_TDK07I',
+    title: 'Mann Mera',
+    artist: 'Gajendra Verma'
+  },
+  {
+    youtubeId: 'V_08pyhaaTo',
+    title: 'Matkar Maya Ko Ahankar',
+    artist: 'Neeraj Arya\'s Kabir Cafe'
+  },
+  {
+    youtubeId: 'LlVxeiIoslk',
+    title: 'Meet',
+    artist: 'Arijit Singh, Sachin-Jigar'
+  },
+  {
+    youtubeId: '2pBZQ927u40',
+    title: 'Noor E Khuda',
+    artist: 'Adnan Sami, Shankar Mahadevan, Shreya Ghoshal'
+  },
+  {
+    youtubeId: 'Etkd-07gnxM',
+    title: 'O Maahi',
+    artist: 'Arijit Singh, Pritam'
+  },
+  {
+    youtubeId: 'Qsvga1oPoeE',
+    title: 'O Sanam',
+    artist: 'Lucky Ali'
+  },
+  {
+    youtubeId: 'Z93rAu25KqI',
+    title: 'Phir Le Aya Dil (Reprise)',
+    artist: 'Arijit Singh, Pritam'
+  },
+  {
+    youtubeId: 'TLEPzT8jrUU',
+    title: 'Piya O Re Piya',
+    artist: 'Atif Aslam, Shreya Ghoshal'
+  },
+  {
+    youtubeId: 'piUHBTXsoiY',
+    title: 'Raabta',
+    artist: 'Arijit Singh, Pritam'
+  },
+  {
+    youtubeId: 'aVPwXQgY2QI',
+    title: 'Raabta (Kehte Hain Khuda Ne)',
+    artist: 'Shreya Ghoshal, Arijit Singh, Pritam'
+  },
+  {
+    youtubeId: 'tbXNVm8CXxg',
+    title: 'Raabta (Siyaah Raatein)',
+    artist: 'Arijit Singh, Pritam'
+  },
+  {
+    youtubeId: 'bkTN0iQ7FSI',
+    title: 'Saaiyaan',
+    artist: 'Kamhan, Salim-Sulaiman'
+  },
+  {
+    youtubeId: 'zfWlGsiFDzk',
+    title: 'Safar',
+    artist: 'Bayaan'
+  },
+  {
+    youtubeId: 'TuUVVKVdZm4',
+    title: 'Saiyyan',
+    artist: 'Kailash Kher'
+  },
+  {
+    youtubeId: 'tpuAUPu6FCA',
+    title: 'Sajna Ve',
+    artist: 'Aditya Rikhari, Ravator'
+  },
+  {
+    youtubeId: 'AFTIVN8rRbI',
+    title: 'Sajni',
+    artist: 'Arijit Singh, Ram Sampath'
+  },
+  {
+    youtubeId: 'pUwpLoLlzNQ',
+    title: 'Samjhawan',
+    artist: 'Arijit Singh, Shreya Ghoshal'
+  },
+  {
+    youtubeId: 'HrnrqYxYrbk',
+    title: 'Satranga',
+    artist: 'Arijit Singh, Shreyas P'
+  },
+  {
+    youtubeId: 'W4sHmzMCo8s',
+    title: 'Saudebazi (Encore)',
+    artist: 'Arijit Singh, Pritam'
+  },
+  {
+    youtubeId: 'GpLj8f2CI00',
+    title: 'Sawaar Loon',
+    artist: 'Monali Thakur, Amit Trivedi'
+  },
+  {
+    youtubeId: 'KptF_0yWr_Q',
+    title: 'Saware',
+    artist: 'Arijit Singh, Pritam'
+  },
+  {
+    youtubeId: 'yEJQpoHfw0s',
+    title: 'Tenu Na Bol Pawaan',
+    artist: 'Yasser Desai, Amjad Nadeem'
+  },
+  {
+    youtubeId: 'lel1CQF15Rw',
+    title: 'Tera Ghata',
+    artist: 'Gajendra Verma'
+  },
+  {
+    youtubeId: 'dgA78WcmH4c',
+    title: 'Tere Bin Nahi Laage',
+    artist: 'Uzair Jaswal'
+  },
+  {
+    youtubeId: 'Fi3GovijpWA',
+    title: 'Tere Bina',
+    artist: 'A.R. Rahman, Chinmayi'
+  },
+  {
+    youtubeId: 'otyKssvCZxw',
+    title: 'Tere Binaa',
+    artist: 'Mustafa Zahid'
+  },
+  {
+    youtubeId: 'KUpwupYj_tY',
+    title: 'Tere Hawaale',
+    artist: 'Arijit Singh, Shilpa Rao, Pritam'
+  },
+  {
+    youtubeId: 'jbtF5YR6hNU',
+    title: 'Tere Naina',
+    artist: 'Shankar Mahadevan, Shankar-Ehsaan-Loy'
+  },
+  {
+    youtubeId: '3R-q79a7n98',
+    title: 'Teri Deewani',
+    artist: 'Kailash Kher'
+  },
+  {
+    youtubeId: 'yiJ5k2vBtlw',
+    title: 'Teri Jhuki Nazar',
+    artist: 'Shafqat Amanat Ali, Pritam'
+  },
+  {
+    youtubeId: 'FGTv9-oQhIg',
+    title: 'Thodi Der',
+    artist: 'Farhan Saeed, Shreya Ghoshal'
+  },
+  {
+    youtubeId: 'AX6OrbgS8lI',
+    title: 'Tu Hai Kahan',
+    artist: 'AUR'
+  },
+  {
+    youtubeId: 'P8PWN1OmZOA',
+    title: 'Tu Jaane Na',
+    artist: 'Atif Aslam, Pritam'
+  },
+  {
+    youtubeId: 'AqVIQ9ymvM8',
+    title: 'Tum Ho',
+    artist: 'Mohit Chauhan, A.R. Rahman'
+  },
+  {
+    youtubeId: 'Feoea8FQTI0',
+    title: 'Tum Prem Ho (Reprise)',
+    artist: 'Mohit Lalwani, Bharat Kamal'
+  },
+  {
+    youtubeId: 'I94fhjQ-U30',
+    title: 'Tum Se Hi',
+    artist: 'Mohit Chauhan, Pritam'
+  },
+  {
+    youtubeId: '1nWQs6IxTrY',
+    title: 'Tum Tak',
+    artist: 'Javed Ali, Pooja Vaidyanath, A.R. Rahman'
+  },
+  {
+    youtubeId: '01HR1uP4kic',
+    title: 'Tune Jo Na Kaha',
+    artist: 'Mohit Chauhan, Pritam'
+  },
+  {
+    youtubeId: '3hq_DhGOzik',
+    title: 'Vaaroon',
+    artist: 'Romy, Anand Bhaskar'
+  },
+  {
+    youtubeId: 'E_SbwSe15y0',
+    title: 'Ve Haaniyaan',
+    artist: 'Danny, Avvy Sra'
+  },
+  {
+    youtubeId: 'TjXH_P7Khhg',
+    title: 'Ve Kamleya',
+    artist: 'Arijit Singh, Shreya Ghoshal'
+  },
+  {
+    youtubeId: '8qdBFcHxiEg',
+    title: 'Wafa Ne Bewafai',
+    artist: 'Arijit Singh, Neeti Mohan'
+  },
+  {
+    youtubeId: 'Iqu_W5W4YO4',
+    title: 'Yeh Fitoor Mera',
+    artist: 'Arijit Singh, Amit Trivedi'
+  },
+  {
+    youtubeId: 'RzSIDoA1ZDw',
+    title: 'Your Eyes Got My Heart Falling For You',
+    artist: 'Barney Sku'
+  },
+  {
+    youtubeId: '-0vjLZtDkq0',
+    title: 'Zindagi Kuch Toh Bata (Reprise)',
+    artist: 'Jubin Nautiyal, Pritam'
   }
 ];
